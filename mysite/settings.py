63e1264adbb44b8c3fa6308caa90eae2b1671f50
Kyle Harrington <harrihlidskjalfar@gmail.com>
2019-09-10 10:24:11 -0600

    Adding new allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*+@uvx%jffwk*#9adx$&c7gc5lpj7h-iyx*j+lk%t%i(y@koh9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'www.serialexperimentskyle.com']
 
 LOGIN_REDIRECT_URL = '/'
 
