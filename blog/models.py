34d6429fb05d47132c340fde59b7d35632d96312
Kyle Harrington <harrihlidskjalfar@gmail.com>
2019-09-12 16:05:22 -0600

    Add markup support for comments

--- blog/models.py
+++ blog/models.py
@@ -30,9 +30,13 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
-    text = models.TextField()
+    text = MarkdownxField()
     created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
+    
+    @property 
+    def formatted_markdown(self):
+        return markdownify(self.text)
 
     def approve(self):
         self.approved_comment = True
