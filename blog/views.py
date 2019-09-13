38c2d07e8c761c493975403a6bf5630306360dd2
Kyle Harrington <harrihlidskjalfar@gmail.com>
2019-09-13 12:40:02 -0600

    Added archive page and limited main page post list

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ from .forms import PostForm, CommentForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    posts = posts[:3]
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -45,6 +46,10 @@ def post_edit(request, pk):
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def archive_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/archive_list.html', {'posts': posts})
 
 @login_required
 def post_publish(request, pk):
