194f63fb43d9cbd83b429cf78b8a29b8500cd7b9
Kyle Harrington <harrihlidskjalfar@gmail.com>
2019-09-18 10:08:29 -0600

    Add staff requirements for critical functionality in prep for other user accts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
@@ -22,6 +23,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 @login_required
+@staff_member_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -35,6 +37,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 @login_required
+@staff_member_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -49,6 +52,7 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 @login_required
+@staff_member_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
@@ -106,6 +110,7 @@ def unsubscribe(request, email, token):
     return render(request, 'blog/unsubscribe.html', {'email':unsubscribed_email})
 
 @login_required
+@staff_member_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
@@ -127,6 +132,7 @@ def post_publish(request, pk):
     return redirect('post_detail', pk=pk)
 
 @login_required
+@staff_member_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
@@ -157,12 +163,14 @@ def add_comment_to_post(request, pk):
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
 @login_required
+@staff_member_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
 
 @login_required
+@staff_member_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
